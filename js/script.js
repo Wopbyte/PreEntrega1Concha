75384f8bfc846b8613b38c04ca594b9f836bfa29
Wladimir <wladi200@gmail.com>
2023-02-22 01:40:25 -0300

    cambios al loop

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let password = '123445';
 let gemas = 10000;
 let bPerm = 2000;
 let bProm = 4000;
-let opc = 0;
 
 function login() {
 
@@ -26,64 +25,72 @@ function login() {
 
 if (login()) {
 
-    let opc = parseInt(prompt("Ingresa el numero de la opcion: \n1) Revisar gemas. \n2) Abonar. \n3) Jugar Banner Permanente \n4) Jugar Banner Promocional. \n5)Salir"));
-
     do {
-
-        if (opc > 5 || opc <= 0) {
-            alert("Ingrese una opcion desde el 1 al 5")
+        opc = parseInt(prompt("Ingresa el numero de la opcion: \n1) Revisar gemas. \n2) Abonar. \n3) Jugar Banner Permanente \n4) Jugar Banner Promocional. \n5)Salir"));
+        if (Number.isNaN(opc)) {
+            alert("El valor ingresado no es un numero");
+            opc = 0;
         }
         else {
-
-            switch (opc) {
-                case 1:
-                    alert("Tu gemas actual es de: " + gemas + " gemas");
-                    break;
-
-                case 2:
-                    let abono = parseInt(prompt("Ingresa el monto a abonar:"))
-                    if (Number.isNaN(abono)) {
-                        alert("El valor ingresado no es un numero");
-                    }
-                    else {
-                        gemas = gemas + abono;
-                        alert("gemas actuales: " + gemas);
-                    }
-                    break;
-
-                case 3:
-                    alert("El valor del banner permanente es de: " + bPerm + " gemas");
-                    if (gemas >= bPerm) {
-                        gemas = gemas - bPerm;
-                        alert("Has jugado al Banner Permanente, te quedan: " + gemas + " gemas");
-                    }
-                    else {
-                        alert("Gemas insuficientes, tienes: " + gemas + " gemas");
-                    }
-                    break;
-
-                case 4:
-                    alert("El valor del banner promocional es de: " + bProm + " gemas");
-                    if (gemas >= bProm) {
-                        gemas = gemas - bProm;
-                        alert("Has jugado al Banner Promocional, te quedan: " + gemas + " gemas");
-                    }
-                    else {
-                        alert("Gemas insuficientes, tienes: " + gemas + " gemas");
-                    }
-                    break;
-
-                default:
-                    alert("La opcion ingresada no es valida, intenta nuevamente");
-                    break;
-
+            if (opc > 5 || opc <= 0) {
+                alert("Ingrese una opcion desde el 1 al 5");
+                opc = 0;
             }
-            
+            else {
+    
+                switch (opc) {
+                    case 1:
+                        alert("Tu gemas actual es de: " + gemas + " gemas");
+                        break;
+    
+                    case 2:
+                        let abono = parseInt(prompt("Ingresa el monto a abonar:"));
+                        if (Number.isNaN(abono)) {
+                            alert("El valor ingresado no es un numero");
+                        }
+                        else {
+                            gemas = gemas + abono;
+                            alert("gemas actuales: " + gemas);
+                        }
+                        break;
+    
+                    case 3:
+                        alert("El valor del banner permanente es de: " + bPerm + " gemas");
+                        if (gemas >= bPerm) {
+                            gemas = gemas - bPerm;
+                            alert("Has jugado al Banner Permanente, te quedan: " + gemas + " gemas");
+                        }
+                        else {
+                            alert("Gemas insuficientes, tienes: " + gemas + " gemas");
+                        }
+                        break;
+    
+                    case 4:
+                        alert("El valor del banner promocional es de: " + bProm + " gemas");
+                        if (gemas >= bProm) {
+                            gemas = gemas - bProm;
+                            alert("Has jugado al Banner Promocional, te quedan: " + gemas + " gemas");
+                        }
+                        else {
+                            alert("Gemas insuficientes, tienes: " + gemas + " gemas");
+                        }
+                        break;
+    
+                    case 5:
+                        alert("Vuleve nuevamente, Adios");
+                        break;
+                    default:
+                        alert("La opcion ingresada no es valida, intenta nuevamente");
+                        break;
+    
+                }
+                
+            }
+    
         }
 
-        opc = parseInt(prompt("Ingresa el numero de la opcion: \n1) Revisar gemas. \n2) Abonar. \n3) Jugar Banner Permanente \n4) Jugar Banner Promocional. \n5)Salir"));
-    } while (opc <= 4)
-    alert("Vuleve nuevamente, Adios");
+    } while (opc < 5)
+    
 
 }
 else {
